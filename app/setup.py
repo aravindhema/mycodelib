ff1080f1c1ad8b7cf2726837a2a197f16a119f86
Hema Aravind <34648329+aravindhema@users.noreply.github.com>
2020-01-25 18:03:44 +0530

    Add files via upload

---
+++ app/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath( path.dirname( __file__ ) )
+
+setup(
+    name='hworldapp',
+    version='1.0.0',
+    description='Python Flask app for Hello World',
+    license='Apache-2.0'
+)
